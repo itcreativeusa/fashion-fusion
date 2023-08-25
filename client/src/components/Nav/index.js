e1fb008ce35d4a2ecd3e9a2291f1ff1348b3165f
otmorales23 <oliverwills0227@gmail.com>
2023-08-25 13:56:36 -0700

    added some styling to navbar and title

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
+        <ul className="navbar">
+          <li className="li">
             <Link to="/orderHistory">Order History</Link>
           </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+          <li className="li">
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
@@ -20,31 +20,36 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <header>
+          <nav>
+            <ul className="navbar">
+              <li className="li">
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li className="li">
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
       );
     }
   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
+    <header className="header">
+      <nav>{showNavigation()}</nav>
+
+      <h1 className="title">
         <Link to="/">
-          <span role="img" aria-label="shopping bag">
+          <span role="img" aria-label="dress">
             👗
           </span>
           Fashion Fusion
         </Link>
       </h1>
-
-      <nav>{showNavigation()}</nav>
     </header>
+      
   );
 }
 
