3c17f7a582baaa8a6c3ba208c306002b0ea5643d
Kristina <kristina@Kristinas-MBP.attlocal.net>
2023-08-25 13:57:37 -0700

    merged main to feature branch

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+      <footer>
+        <small>© 2023 Fashion Fusion development. All rights reserved.</small>
+      </footer>
+    );
+};
