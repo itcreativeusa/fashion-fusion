0ec4a74ece8f20b553f4ede782719dae0d9559cf
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-31 08:58:44 -0700

    add publi and secret key

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -15,7 +15,9 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 
-const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+const stripePromise = loadStripe(
+  "pk_test_51Nl1V0IUgxhWhAI19CoN4iBAWLw9jq0krwfCKLohPa734YUEzVM5oz15zqX2vRuq2tvOf4TvCg9V0TZycDeaRRpU004SHwoa2S"
+);
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
